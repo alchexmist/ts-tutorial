5a0e22501d2f22d08e98f5cb095f6d8e5d91c265
Alberto Seoane Martínez <fun2src@riazor.local>
2024-03-09 19:47:13 +0100

    Trivial changes.

--- hello.js
+++ hello.js
@@ -1,6 +1,15 @@
 "use strict";
 console.log('Hello, Albert!');
-function greet(person, date) {
-    console.log(`Hello, ${person}, today is ${date}`);
+function greet(id, person, date) {
+    console.log(`Hello, ${person} with id: ${id}, today is ${date}`);
 }
-greet("Albert", new Date());
+greet(1, "Albert", new Date());
+function double(x) {
+    console.log(2 * x);
+}
+;
+double(3.14);
+// Evitar errores de comprobación de tipos: any
+let obj = { name: "albert" };
+console.log(obj.name);
+console.log(obj.surname);
--- hello.ts
+++ hello.ts
@@ -2,8 +2,19 @@
 
 console.log('Hello, Albert!');
 
-function greet(person: string, date: Date) {
-    console.log(`Hello, ${person}, today is ${date}`);
+function greet(id: number, person: string, date: Date) {
+    console.log(`Hello, ${person} with id: ${id}, today is ${date}`);
 }
 
-greet("Albert", new Date());
+greet(1, "Albert", new Date());
+
+function double(x: number) {
+    console.log(2 * x);
+};
+
+double(3.14);
+
+// Evitar errores de comprobación de tipos: any
+let obj: any = { name: "albert" };
+console.log(obj.name);
+console.log(obj.surname);
