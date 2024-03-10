bebf8f53b28e617998889b61f2f1821f3a1166ea
Alberto Seoane Martínez <fun2src@riazor.local>
2024-03-10 15:50:10 +0100

    Union types.

--- 2-union-type.ts
+++ 2-union-type.ts
@@ -1,6 +1,19 @@
 function printID(id:number|string) {
     console.log(`Your ID is ${id}`);
-}
+};
 
 printID(666);
 printID("alchexmist");
+
+function welcome(id: string|string[]|number) {
+    if(typeof id === "string") {
+        console.log(`Welcome, ${id.toUpperCase()}.`);
+    } else if(Array.isArray(id)) {
+        console.log(`Welcome, ${id.join(" & ")}.`);
+    } else {
+        console.log(`Welcome, ${id}.`);
+    }
+};
+
+welcome(666);
+welcome(['Jack', 'The Ripper']);
